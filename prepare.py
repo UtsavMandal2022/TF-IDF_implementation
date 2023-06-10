b2e8b3c22332b3de3481ed01ca1d3e19bd0b028a
UtsavMandal2022 <utsav2016mandal@gmail.com>
2023-06-11 01:07:36 +0530

    c1

---
+++ prepare.py
@@ -0,0 +1,60 @@
+
+with open('Qdata/output_headings.txt','r') as f:
+    lines=f.readlines()
+
+# for ind,line in enumerate(lines):
+#     print(ind,line)
+
+def preprocess(document_text):
+    # remove the leading numbers from the string, remove not alpha numeric characters, make everything lowercase
+    terms = [term.lower() for term in document_text.strip().split()[1:]]
+    return terms
+
+vocab={}
+docs=[]
+
+for line in lines:
+    doc = preprocess(line)
+    docs.append(doc)
+    for term in doc:
+        if term not in vocab:
+            vocab[term]=1
+        else:
+            vocab[term]+=1
+
+# print(len(docs))
+# print(docs[2156])
+# print(len(vocab))
+# print(vocab)
+
+vocab = dict(sorted(vocab.items(), key=lambda item: item[1], reverse=True))
+
+with open ('vocab.txt','w') as f:
+    for key in vocab:
+        f.write(key+'\n')
+
+with open ('idf-values.txt','w') as f:
+    for key in vocab:
+        f.write(str(vocab[key])+'\n')
+
+with open ('docs.txt','w') as f:
+    for doc in docs:
+        for term in doc:
+            f.write(term+' ')
+        f.write('\n')
+
+inv_index={}
+
+for ind,doc in enumerate(docs):
+    for term in doc:
+        if term not in inv_index:
+            inv_index[term]=[ind]
+        else:
+            inv_index[term].append(ind)
+
+with open ('inv-index.txt','w') as f:
+    for key in inv_index:
+        f.write(key+'\n')
+        for value in inv_index[key]:
+            f.write(str(value)+' ')
+        f.write('\n')
---
+++ query_heading.py
@@ -0,0 +1,92 @@
+import math
+
+def load_vocab():
+    vocab = {}
+    with open('vocab.txt', 'r') as f:
+        lines = f.readlines()
+    with open ('idf-values.txt','r') as f:
+        idfs = f.readlines()
+
+    for (line, idf) in zip(lines, idfs):   
+        vocab[line.strip()] = int(idf.strip())
+
+    return vocab
+
+def load_docs():
+    docs = []
+    with open('docs.txt', 'r') as f:
+        lines = f.readlines()
+    for line in lines:
+        docs.append(line.strip().split())
+    print("Doc size: ",len(docs))
+    print("Sample doc: ",docs[216])
+    return docs
+
+def load_inv_index():
+    inv_index = {}
+    with open('inv-index.txt', 'r') as f:
+        lines = f.readlines()
+    for i in range(0, len(lines), 2):
+        inv_index[lines[i].strip()] = lines[i+1].strip().split()
+    print("Inv index size: ",len(inv_index))
+    return inv_index
+
+vocab={}
+docs=[]
+inv_index={}
+vocab = load_vocab()
+docs = load_docs()
+inv_index = load_inv_index()
+
+def get_tf(term):
+    tf = {}
+    if term in inv_index:
+        for ii in inv_index[term]:
+            if ii not in tf:
+                tf[ii] = 1
+            else:
+                tf[ii] += 1
+
+    for key in tf:
+        tf[key]/=len(docs[int(key)])
+    return tf
+
+def get_idf(term):
+    if term in vocab:
+        return math.log(len(docs)/vocab[term])
+    else:
+        return 0
+    
+def get_potential_docs(query):
+    potential_docs = {}
+    for term in query:
+        if term not in vocab:
+            continue
+        tf_val=get_tf(term)
+        idf_val=get_idf(term)
+        print(term, tf_val, idf_val)
+        for key in tf_val:
+            if key not in potential_docs:
+                potential_docs[key]=tf_val[key]*idf_val
+            else:
+                potential_docs[key]+=tf_val[key]*idf_val
+    
+    for key in potential_docs:
+        potential_docs[key]/=len(query)
+
+    potential_docs = dict(sorted(potential_docs.items(), key=lambda item: item[1], reverse=True))
+
+    return potential_docs
+
+query_term=input('Enter the query term: ')
+query=[term.lower() for term in query_term.strip().split()]
+print(query)
+
+links=[]
+with open('Qdata/q_index.txt','r') as f:
+    links=f.readlines()
+# print(len(links))
+
+potential_docs = get_potential_docs(query)
+for key in potential_docs:
+    print("Document: ",docs[int(key)],links[int(key)],"Score: ", potential_docs[key])
